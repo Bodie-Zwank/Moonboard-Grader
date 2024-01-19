4d8ad2537607f9758798f77786590584be7e8f09
Bodie-Zwank <116914666+Bodie-Zwank@users.noreply.github.com>
2024-01-19 09:57:41 -0600

    Update networks.py

--- nn_scratch/networks.py
+++ nn_scratch/networks.py
@@ -1,3 +1,4 @@
+# ignore
 class RNN:
     def __init__(self, recurrent_layer, recurrent_activation, dense_layer, dense_activation, dense_layer2, final_activation_layer):
         self.recurrent_layer = recurrent_layer
